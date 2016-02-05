ddc8f8412f4eda8d5e8fdb0cb67e762f3792d327
Kamil Owczarz <kamilo2602+git@gmail.com>
2016-02-05 22:54:33 +0100

    uładniony kod

--- app/beginnigpage.php
+++ app/beginnigpage.php
@@ -4,54 +4,57 @@
 
 ?>
 
-<!DOCTYPE html>
-<html>
-<head>
-        
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    
-    <title>Welcome to Gówno</title>
+    <!DOCTYPE html>
+    <html>
 
-    <link rel="Stylesheet" type="text/css" href="../style.css" />
-    
-	<link href='https://fonts.googleapis.com/css?family=Inconsolata&subset=latin-ext,latin' rel='stylesheet' type='text/css'>
-    
-</head>
-<body>
-    <div id="calosc2" >
-        <div id="sub_site1">
-            
+    <head>
+
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+        <title>Welcome to Gówno</title>
+
+        <link rel="Stylesheet" type="text/css" href="../style.css" />
+
+        <link href='https://fonts.googleapis.com/css?family=Inconsolata&subset=latin-ext,latin' rel='stylesheet' type='text/css'>
+
+    </head>
+
+    <body>
+        <div id="calosc2">
+            <div id="sub_site1">
+
+            </div>
+            <div id="sub_site2">
+
+            </div>
+            <div id="sub_site3">
+
+            </div>
+            <div id="sub_site4">
+                <div id="registration">
+                    <div id="formInReg">
+                        <form>
+                            <div class="formInput">
+                                Twój adres email:
+                            </div>
+                            <div>
+                                <input name="e-mail" type="email" class="formInput">
+                            </div>
+                            <div class="formInput">
+                                Hasło:
+                            </div>
+                            <div>
+                                <input name="pass" type="password" class="formInput">
+                            </div>
+                            <div class="center">
+                                <input type="submit" onmouseover="img.src=img/reg1.png" onmouseout="img.src=img/reg2.png">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div id="sub_site2">
-            
-        </div>
-        <div id="sub_site3">
-            
-        </div>
-        <div id="sub_site4">
-            <div id="registration">
-                 <div id="formInReg">
-                     <form >
-                         <div class="formInput">
-                            Twój adres email:
-                         </div>
-                         <div>
-                             <input name="e-mail" type="email" class="formInput">
-                         </div>
-                         <div class="formInput">
-                             Hasło:
-                         </div>
-                         <div>
-                             <input name="pass" type="password" class="formInput">
-                         </div>
-                         <div class="center">
-                             <input type="submit" onmouseover="img.src=img/reg1.png" onmouseout="img.src=img/reg2.png">
-                         </div>
-                    </form>
-                  </div>
-              </div>
-        </div>
-    </div>
-</body>
-</html>
+    </body>
+
+    </html>
